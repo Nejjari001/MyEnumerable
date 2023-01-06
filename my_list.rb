db0a96dba6f26640b072941c25588a4bee26e4b3
Ajayi Ridwan <ridwanullahiajayi01@gmail.com>
2023-01-06 10:21:44 +0100

    Completed code

--- my_list.rb
+++ my_list.rb
@@ -7,4 +7,22 @@ def initialize(*list)
     @list = list
   end
 
-  
+ def each
+   yield @list
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+
+list.all? { |e| e < 5 }
+# => true
+list.all? { |e| e > 5 }
+# => false
+
+list.any? { |e| e == 2 }
+# => true
+list.any? { |e| e == 5 }
+# => false
+
+list.filter(&:even?)
+# => [2, 4]
